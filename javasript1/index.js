0f4022f51972f40fe1a85047dfe21afbbcd97cea
Anurag1707 <Anuragjhariya1707@gmail.com>
2026-05-11 13:12:16 +0530

    lect1

---
+++ javasript1/index.js
@@ -0,0 +1,10 @@
+
+
+// let btn = document.querySelector("button")
+// let btn = document.getElementById("one")
+let btn = document.getElementsByClassName("hi")
+let body = document.querySelector("body")
+btn.addEventListener("click",function(){
+    body.style.backgroundColor="red"
+})
+
