244cfb284b2a002366fc13e9c79d91247eb3de7f
Anurag1707 <Anuragjhariya1707@gmail.com>
2026-06-02 15:25:11 +0530

    1

--- javascript1/index.js
+++ javascript1/index.js
@@ -1,10 +1,10 @@
 
 
-// let btn = document.querySelector("button")
+let btn = document.querySelector("button")
 // let btn = document.getElementById("one")
-let btn = document.getElementsByClassName("hi")
+//let btn = document.getElementsByClassName("hi")
 let body = document.querySelector("body")
 btn.addEventListener("click",function(){
-    body.style.backgroundColor="red"
+    body.style.backgroundColor="pink"
 })
 
